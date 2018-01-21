c2020fc0909b04c62d6b9597583c777a303a9269
poopierosary <kevinsaelee@live.com>
2018-01-20 18:23:48 -0800

    added styles to calculator.

--- calculator/src/scripts/cal.js
+++ calculator/src/scripts/cal.js
@@ -1,3 +1,6 @@
 function Calc(val) {
-	alert(val);
+	if(val != "=")
+		document.form.txtval.value = document.form.txtval.value + val;
+	else
+		document.form.txtval.value = eval(document.form.txtval.value);
 }
